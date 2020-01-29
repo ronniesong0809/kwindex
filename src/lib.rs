5175fda586e67031e11a9d935d8980b6403d88f6
Ronnie Song <ronniesong0809@gmail.com>
2020-01-29 12:29:00 -0800

    feat: add necessary struct and function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Debug, Default, Clone)]
+pub struct KWIndex<'a>(Vec<&'a str>);
+
+// Make a new empty target words list.
+pub fn new() -> Self {
+
+}
+
+// Parse the `target` text and add the sequence of
+// valid words contained in it to this `KWIndex`
+// index.
+pub fn extend_from_text(mut self, target: &'a str) -> Self {
+
+}
+
+// Count the number of occurrences of the given `keyword`
+// that are indexed by this `KWIndex`.
+pub fn count_matches(&self, keyword: &str) -> usize {
+
+}
+
+// Count the number of words that are indexed by this
+// `KWIndex`.
+pub fn len(&self) -> usize {
+
+}
+
+// Is this index empty?
+pub fn is_empty(&self) -> bool {
+    
 }
