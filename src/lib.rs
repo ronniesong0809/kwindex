333e144749f7b9fd1366cb79287457d517923af1
Ronnie Song <ronniesong0809@gmail.com>
2020-01-30 15:59:00 -0800

    feat: add implementations on type and add new()

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,37 @@
 #[derive(Debug, Default, Clone)]
-pub struct KWIndex<'a>(Vec<&'a str>);
-
-// Make a new empty target words list.
-pub fn new() -> Self {
-
+pub struct KWIndex<'a str> {
+    word: Vec<&'a str>,
 }
 
-// Parse the `target` text and add the sequence of
-// valid words contained in it to this `KWIndex`
-// index.
-pub fn extend_from_text(mut self, target: &'a str) -> Self {
+impl<'a str> KWIndex<'a str> {
+    // Make a new empty target words list.
+    pub fn new() -> Self {
+        let word = Vec::new();
+        Self { word }
 
-}
+    }
 
-// Count the number of occurrences of the given `keyword`
-// that are indexed by this `KWIndex`.
-pub fn count_matches(&self, keyword: &str) -> usize {
+    // Parse the `target` text and add the sequence of
+    // valid words contained in it to this `KWIndex`
+    // index.
+    // pub fn extend_from_text(mut self, target: &'a str) -> Self {
 
-}
+    // }
 
-// Count the number of words that are indexed by this
-// `KWIndex`.
-pub fn len(&self) -> usize {
+    // Count the number of occurrences of the given `keyword`
+    // that are indexed by this `KWIndex`.
+    // pub fn count_matches(&self, keyword: &str) -> usize {
 
-}
+    // }
 
-// Is this index empty?
-pub fn is_empty(&self) -> bool {
-    
+    // Count the number of words that are indexed by this
+    // `KWIndex`.
+    pub fn len(&self) -> usize {
+        self.len()
+    }
+
+    // Is this index empty?
+    pub fn is_empty(&self) -> bool {
+        self.len()==0
+    }
 }
