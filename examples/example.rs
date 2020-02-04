b3287b23c6ba6d30327c50eabf45715086caad3b
Ronnie Song <ronniesong0809@gmail.com>
2020-02-03 16:16:17 -0800

    refactor: optimize nested loops, fix a bug, and fix examples

--- examples/example.rs
+++ examples/example.rs
@@ -2,7 +2,10 @@ extern crate kwindex;
 
 fn main() {
     let mut index = kwindex::KWIndex::new();
+    println!("is_empty(): {}", index.is_empty());
     index = index.extend_from_text("Hey world!");
+    println!("is_empty(): {}", index.is_empty());
     println!("len(): {}", index.len());
     println!("count_matches(): {}", index.count_matches("world"));
+    println!("{:?}", index);
 }
